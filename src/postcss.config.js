540f3d2c5c1c4cfe3e45a605a7e9d5a21deebec1
podyankee <podyankee@gmail.com>
2019-10-13 20:06:49 +0300

    svg icons repeared

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('css-mqpacker'),
+		require('css-mqpacker'),
+		require('postcss-svg'),
     require('cssnano')({
       preset: [
         'default', {
           discardComments: {
             removeAll: true
-          }
-        }
+					},
+					svgo: false
+				},
       ]
     })
   ]
