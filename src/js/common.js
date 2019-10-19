797a27648ce71f625fc7a34600ee1639e829cea6
podyankee <podyankee@gmail.com>
2019-10-19 04:19:18 +0300

    section surf added

--- src/js/common.js
+++ src/js/common.js
@@ -5,11 +5,25 @@ $(document).ready(function() {
 				prevArrow:
 				'<svg class="icon icon-arrow-left slider-arrow slider-arrow__left"><use xlink: href = "assets/img/sprite.svg#arrow-left"></use></svg>',
 				nextArrow: '<svg class="icon icon-arrow-right slider-arrow slider-arrow__right"><use xlink: href = "assets/img/sprite.svg#arrow-right"></use></svg>',
-				asNavFor: '.slider-dots'
+				asNavFor: '.slider-dotshead'
 		});
-		$('.slider-dots').slick({
+		$('.slider-dotshead').slick({
 			slidesToShow: 4,
 			slidesToScroll: 4,
 			asNavFor: '.header__slider'
+		});
+		$('.surf-slider').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			prevArrow:
+				'<svg class="icon icon-arrow-left slider-arrow slider-arrow__left"><use xlink: href = "assets/img/sprite.svg#arrow-left"></use></svg>',
+				nextArrow: '<svg class="icon icon-arrow-right slider-arrow slider-arrow__right"><use xlink: href = "assets/img/sprite.svg#arrow-right"></use></svg>',
+				asNavFor: '.slider-map'
+		});
+		$('.slider-map').slick({
+			slidesToShow: 8,
+			slidesToScroll: 1,
+			asNavFor: '.surf-slider',
+			focusOnSelect: true
 		});
 	});
